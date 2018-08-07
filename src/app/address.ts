888f6d13f068dd741daebfca7d923e33a65334c2
Ole MORTENSEN <ole.mortensen@me.com>
2018-08-07 15:48:25 +0200

    add source files

---
+++ src/app/address.ts
@@ -0,0 +1,8 @@
+export class Address {
+  constructor(
+    public firstname?: string,
+    public lastname?: string,
+    public street?: string,
+    public city?: string,
+    public postalcode?: string) {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MyFormComponent } from './my-form/my-form.component';
+import {MatInputModule, MatButtonModule } from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
