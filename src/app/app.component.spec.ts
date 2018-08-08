50bdd79376a208cca0d68d9cb1bf3cd3c0f3a82f
Ole MORTENSEN <ole.mortensen@me.com>
2018-08-08 12:40:28 +0200

    simplify tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import {TestBed, async} from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {MatFormFieldModule, MatInputModule} from '@angular/material';
@@ -6,6 +6,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MyFormComponent} from './my-form/my-form.component';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -15,28 +19,23 @@ describe('AppComponent', () => {
       imports: [FormsModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule]
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
-  it('should have street text element'), async( () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('text[name="street"]')).toBeTruthy();
-  });
-  it('should have address input elements', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    ['firstname', 'lastname', 'city', 'postalcode'].forEach(e => {
-      expect(compiled.querySelector(`input[name="${e}"]`)).toBeTruthy();
-    });
+  it('should contain a form', async(() => {
+    expect(compiled.querySelector('form')).toBeTruthy();
+
   }));
+
 });
--- src/app/my-form/my-form.component.spec.ts
+++ src/app/my-form/my-form.component.spec.ts
@@ -8,6 +8,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 describe('MyFormComponent', () => {
   let component: MyFormComponent;
   let fixture: ComponentFixture<MyFormComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,9 +22,21 @@ describe('MyFormComponent', () => {
     fixture = TestBed.createComponent(MyFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
-  it('should create', () => {
+  it('should create the form', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should have address input elements', async(() => {
+    ['firstname', 'lastname', 'city', 'postalcode'].forEach(e => {
+      expect(compiled.querySelector(`input[name="${e}"]`)).toBeTruthy();
+    });
+  }));
+
+  it('should have street textarea element', async(() => {
+    expect(compiled.querySelector('textarea[name="street"]')).toBeTruthy();
+  }));
 });
